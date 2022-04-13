de416e36e8e7b2c78daa2272bdd3ae5c00872a2f
zepatrik <zepatrik@users.noreply.github.com>
2022-04-13 13:03:50 +0300

    fix: run script should always take local keto

--- examples/run.sh
+++ examples/run.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 set -euo pipefail
 
-PATH="$PATH:$(pwd)/.bin"
-
 if [ ! -f ".bin/keto" ]; then
   mkdir ".bin"
   bash <(curl https://raw.githubusercontent.com/ory/keto/master/install.sh) -b .bin v0.7.0-alpha.1
@@ -13,7 +11,7 @@ function teardown() {
 }
 trap teardown EXIT
 
-keto serve -c ./keto.yml &
+.bin/keto serve -c ./keto.yml &
 keto_server_pid=$!
 
 npx nodemon server.ts
